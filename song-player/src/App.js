2b9624a28ede776c3d2aedafdfcc9b49f8ae1f9f
Jonas Lachowitzer <jonas.lachowitzer@disy.net>
2024-06-12 19:39:01 +0200

    add looping to Player and 2 effects autowah (needs work) and reverb

--- song-player/src/App.js
+++ song-player/src/App.js
@@ -12,6 +12,8 @@ function App() {
     const [player, setPlayer] = useState(null);
     const [audioContextStarted, setAudioContextStarted] = useState(false);
     const [pitchShift, setPitchShift] = useState(null);
+    const [autoWah, setAutoWah] = useState(null);
+    const [reverb, setReverb] = useState(null);
 
     // Starte den Audio-Kontext, falls nicht bereits gestartet
     const startAudioContextIfNeeded = () => {
@@ -25,7 +27,9 @@ function App() {
     const createPlayer = (song) => {
         const audioFile = process.env.PUBLIC_URL + '/songs/' + song;
         const newPlayer = new Tone.Player({
-            url: audioFile, onload: () => {
+            url: audioFile, 
+            loop: true,
+            onload: () => {
                 startAudioContextIfNeeded(); // Starte den Audio-Kontext, wenn der Buffer geladen ist
                 newPlayer.start();
             },
@@ -35,6 +39,7 @@ function App() {
 
     // Funktion zum Ändern der Geschwindigkeit zwischen 0.2 und 1
     const executeSpeedChange = (percentCorrect) => {
+        console.log(`speedChange: ${percentCorrect}`);
         if (player) {
             const minSpeed = 0.2;
             const maxSpeed = 1.0;
@@ -44,6 +49,7 @@ function App() {
     };
     // Funktion zum Ändern der Tonhöhe zwischen 0 und 20
     const executePitchChange = (percentCorrect) => {
+        console.log(`pitchChange: ${percentCorrect}`);
         if (pitchShift) {
             const minPitch = 0;
             const maxPitch = 20;
@@ -52,8 +58,11 @@ function App() {
         }
     };
     // ToDo weitere Funktion, die den Song verändert
-    const executeFunction2 = (percentCorrect) => {
-        console.log(`Function 2: ${percentCorrect}`);
+    const executeAutoWah = (percentCorrect) => {
+        console.log(`autoWah: ${percentCorrect}`);
+        if (autoWah) {
+            autoWah.wet.value = (percentCorrect / 100);
+        }
     };
     // ToDo weitere Funktion, die den Song verändert
     const executeFunction3 = (percentCorrect) => {
@@ -68,12 +77,15 @@ function App() {
         console.log(`Function 5: ${percentCorrect}`);
     };
     // ToDo weitere Funktion, die den Song verändert
-    const executeFunction6 = (percentCorrect) => {
-        console.log(`Function 6: ${percentCorrect}`);
+    const executeReverb = (percentCorrect) => {
+        console.log(`reverb: ${percentCorrect}`);
+        if (reverb) {
+            reverb.wet.value = 1 - (percentCorrect / 100);
+        }
     };
 
     // Erstelle ein Array von Funktionsreferenzen
-    const functionsForSongManipulation = [executeSpeedChange, executePitchChange, executeFunction2, executeFunction3, executeFunction4, executeFunction5, executeFunction6];
+    const functionsForSongManipulation = [executeSpeedChange, executePitchChange, executeAutoWah, executeFunction3, executeFunction4, executeFunction5, executeReverb];
 
     // Funktion zum Ändern der Lautstärke
     const handleVolumeChange = (e) => {
@@ -93,14 +105,34 @@ function App() {
         if (pitchShift) {
             pitchShift.dispose();// Alten Pitch Shift entsorgen, um Speicherleck zu vermeiden
         }
+        if (autoWah) {
+            autoWah.dispose();// Alten AutoWah entsorgen, um Speicherleck zu vermeiden
+        }
+        if (reverb) {
+            reverb.dispose();// Alten Reverb entsorgen, um Speicherleck zu vermeiden
+        }
 
         setPlayer(newPlayer);
 
         // Erstellen des PitchShift-Effekts und Verbinden mit dem Player
-        const newPitchShift = new Tone.PitchShift().toDestination();
-        newPlayer.connect(newPitchShift);
+        const newPitchShift = new Tone.PitchShift();
         setPitchShift(newPitchShift);
 
+        // Erstellen des AutoWah-Effekts und Verbinden mit dem Player
+        const newAutoWah = new Tone.AutoWah({
+            baseFrequency: 5,
+            octaves: 12,
+            sensitivity: -40
+        })
+        setAutoWah(newAutoWah);
+
+        // Erstellen des AutoWah-Effekts und Verbinden mit dem Player
+        const newReverb = new Tone.Reverb({
+            decay: 20,
+        })
+        setReverb(newReverb);
+
+        newPlayer.chain(newPitchShift, newAutoWah, newReverb, Tone.Destination);
         startAudioContextIfNeeded(); // Starte den Audio-Kontext bei Benutzerinteraktion
     }
 
