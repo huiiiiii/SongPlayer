0327391304fd87378cb1c255f2d66461e958657c
Jonas Lachowitzer <jonas.lachowitzer@disy.net>
2024-06-20 00:36:11 +0200

    add two additional initial Grids

--- song-player/src/InitialGrids.js
+++ song-player/src/InitialGrids.js
@@ -1,5 +1,142 @@
 // src/initialGrids.js
 
+// riddle id: 6,123,640
+export const initialGrid3 = [
+    [
+        {type: 'end', rotation: 0},
+        {type: 'straight', rotation: 90},
+        {type: 'straight', rotation: 90},
+        {type: 't', rotation: 270},
+        {type: 'straight', rotation: 0},
+        {type: 'straight', rotation: 0},
+        {type: 'end', rotation: 270}
+    ],
+    [
+        {type: 'end', rotation: 90},
+        {type: 'end', rotation: 90},
+        {type: 'elbow', rotation: 0},
+        {type: 't', rotation: 0},
+        {type: 't', rotation: 270},
+        {type: 'straight', rotation: 90},
+        {type: 'end', rotation: 270}
+    ],
+    [
+        {type: 'straight', rotation: 90},
+        {type: 'end', rotation: 180},
+        {type: 't', rotation: 270},
+        {type: 'straight', rotation: 0},
+        {type: 'elbow', rotation: 90},
+        {type: 'end', rotation: 180},
+        {type: 'end', rotation: 0}
+    ],
+    [
+        {type: 't', rotation: 180},
+        {type: 'straight', rotation: 0},
+        {type: 't', rotation: 270},
+        {type: 'start', rotation: 270},
+        {type: 'end', rotation: 90},
+        {type: 'end', rotation: 90},
+        {type: 't', rotation: 90}
+    ],
+    [
+        {type: 'straight', rotation: 180},
+        {type: 'elbow', rotation: 0},
+        {type: 't', rotation: 180},
+        {type: 't', rotation: 90},
+        {type: 't', rotation: 90},
+        {type: 'straight', rotation: 90},
+        {type: 'elbow', rotation: 90}
+    ],
+    [
+        {type: 'straight', rotation: 90},
+        {type: 'straight', rotation: 0},
+        {type: 'elbow', rotation: 270},
+        {type: 't', rotation: 180},
+        {type: 'elbow', rotation: 270},
+        {type: 'end', rotation: 90},
+        {type: 'end', rotation: 0}
+    ],
+    [
+        {type: 'end', rotation: 180},
+        {type: 'end', rotation: 270},
+        {type: 'end', rotation: 0},
+        {type: 'elbow', rotation: 180},
+        {type: 'elbow', rotation: 0},
+        {type: 't', rotation: 180},
+        {type: 'elbow', rotation: 270}
+    ],
+];
+
+
+
+// riddle id: 2,662,197 (not correctly rotated)
+export const initialGrid2 = [
+    [
+        {type: 'end', rotation: 0},
+        {type: 'straight', rotation: 0},
+        {type: 't', rotation: 0},
+        {type: 'end', rotation: 180},
+        {type: 'end', rotation: 270},
+        {type: 'elbow', rotation: 0},
+        {type: 'end', rotation: 0}
+    ],
+    [
+        {type: 'end', rotation: 90},
+        {type: 'end', rotation: 270},
+        {type: 't', rotation: 180},
+        {type: 't', rotation: 270},
+        {type: 'straight', rotation: 90},
+        {type: 'elbow', rotation: 270},
+        {type: 'straight', rotation: 90}
+    ],
+    [
+        {type: 't', rotation: 180},
+        {type: 'end', rotation: 0},
+        {type: 'end', rotation: 0},
+        {type: 'straight', rotation: 180},
+        {type: 'elbow', rotation: 0},
+        {type: 'straight', rotation: 270},
+        {type: 'elbow', rotation: 90}
+    ],
+    [
+        {type: 'elbow', rotation: 0},
+        {type: 't', rotation: 0},
+        {type: 't', rotation: 270},
+        {type: 'start', rotation: 90},
+        {type: 't', rotation: 0},
+        {type: 't', rotation: 180},
+        {type: 'elbow', rotation: 0}
+    ],
+    [
+        {type: 'end', rotation: 180},
+        {type: 't', rotation: 180},
+        {type: 'end', rotation: 180},
+        {type: 'elbow', rotation: 90},
+        {type: 'end', rotation: 270},
+        {type: 'straight', rotation: 0},
+        {type: 'straight', rotation: 0}
+    ],
+    [
+        {type: 'elbow', rotation: 180},
+        {type: 't', rotation: 180},
+        {type: 't', rotation: 270},
+        {type: 'elbow', rotation: 180},
+        {type: 'end', rotation: 90},
+        {type: 'elbow', rotation: 0},
+        {type: 'straight', rotation: 270}
+    ],
+    [
+        {type: 'end', rotation: 180},
+        {type: 'elbow', rotation: 270},
+        {type: 't', rotation: 270},
+        {type: 'straight', rotation: 0},
+        {type: 'straight', rotation: 180},
+        {type: 'end', rotation: 180},
+        {type: 'end', rotation: 180}
+    ],
+];
+
+// riddle id: ???
 export const initialGrid1 = [
     [
         {type: 'elbow', rotation: 180},
@@ -64,13 +201,4 @@ export const initialGrid1 = [
         {type: 'end', rotation: 180},
         {type: 'elbow', rotation: 180}
     ],
-];
-
-//ToDo Du kannst hier weitere Grids definieren
-export const initialGrid2 = [
-    // ... (Ein anderes Grid-Szenario)
-];
-
-export const initialGrid3 = [
-    // ... (Noch ein anderes Grid-Szenario)
 ];
